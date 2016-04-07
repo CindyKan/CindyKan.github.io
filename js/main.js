98f413ddc09859fbd2c34e424f7df91401022c05
甘昕立 <cindycheerup@gmail.com>
2016-04-07 16:57:38 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=9ff284cf-9447-496a-b0db-0d9e60fbb2e0";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b";
 
 function getData(cb) {
   $.ajax({
